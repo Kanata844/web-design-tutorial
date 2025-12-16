096d98171a67f8af012137737c524883d7057ce5
Kanata844 <ta3wata@gmail.com>
2025-12-16 23:30:06 +0900

    ヘッダーを仮追加

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+    <div class="header-background">
+        <p class=title>ヘッダー</p>
+    </div>
+</header>
+
+<style>
+    .header-background{
+        position: sticky;
+        top: 0;
+        background-color: cyan;
+        width: 100%;
+        height: 80px;
+    }
+    .title{
+        color: white;
+    }
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import {resolve} from "$app/paths"
+    import Header from "$lib/components/Header.svelte";
 </script>
 
+<Header/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="{resolve("/test")}">テストのページ</a> to read the documentation</p>
+
