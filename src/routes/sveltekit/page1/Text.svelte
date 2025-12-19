122ccda176ae8b83c1101e7b896cd44507e9c80e
Kanata844 <ta3wata@gmail.com>
2025-12-19 23:41:41 +0900

    少し追記

--- src/routes/sveltekit/page1/Text.svelte
+++ src/routes/sveltekit/page1/Text.svelte
@@ -1,7 +1,7 @@
 <div>
-    <h1>プロジェクト作成、GitHub Pagesにデプロイ</h1>
-    <p>このページでは、SvelteKitでプロジェクトを作り、とりあえず公開するところまでやります。ut.code(); Learnの<a href="https://learn.utcode.net/docs/web-servers/wsl-setup/">WSLのセットアップ(Windowsのみ)</a><a href="https://learn.utcode.net/docs/web-servers/linux-commands/">Linuxコマンド</a><a href="https://learn.utcode.net/docs/web-servers/node-js/">はじめてのNode.js</a>を確認して、(Widnowsの方はWSLと、)Node.jsをインストールしてください。また、<a href="https://learn.utcode.net/docs/web-servers/git-github-init/">GitとGitHubのセットアップ</a>の内容も終わらせておいてください。</p>
-    <p><a href="https://learn.utcode.net/docs/web-servers/module/">モジュールとnpm</a></p>
+    <h1>プロジェクト作成</h1>
+    <p>このページでは、SvelteKitでプロジェクトを作り、GitHubのリポジトリを作るところまでやります。ut.code(); Learnの<a href="https://learn.utcode.net/docs/web-servers/wsl-setup/">WSLのセットアップ(Windowsのみ)</a><a href="https://learn.utcode.net/docs/web-servers/linux-commands/">Linuxコマンド</a><a href="https://learn.utcode.net/docs/web-servers/node-js/">はじめてのNode.js</a>を確認して、(Widnowsの方はWSLと、)Node.jsをインストールしてください。また、<a href="https://learn.utcode.net/docs/web-servers/git-github-init/">GitとGitHubのセットアップ</a>の内容も終わらせておいてください。</p>
+    <p><a href="https://learn.utcode.net/docs/web-servers/module/">モジュールとnpm</a><a href="https://learn.utcode.net/docs/web-servers/git/">Gitを用いたバージョン管理</a><a href="https://learn.utcode.net/docs/web-servers/github/">GitとGithubを用いた共同開発</a>の知識も使うので、わからなくなったら参考にしてください。</p>
 </div>
 
 <style>
