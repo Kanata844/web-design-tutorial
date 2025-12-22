7f7980374f8cc19926a5c25df23df635d3c8e538
Kanata844 <ta3wata@gmail.com>
2025-12-22 13:09:07 +0900

    作業を進めた

--- src/routes/sveltekit/page1/Text.svelte
+++ src/routes/sveltekit/page1/Text.svelte
@@ -1,7 +1,14 @@
+<script lang="ts">
+    import picture1 from "./picture1.png"
+</script>
+
 <div>
     <h1>プロジェクト作成</h1>
     <p>このページでは、SvelteKitでプロジェクトを作り、GitHubのリポジトリを作るところまでやります。ut.code(); Learnの<a href="https://learn.utcode.net/docs/web-servers/wsl-setup/">WSLのセットアップ(Windowsのみ)</a><a href="https://learn.utcode.net/docs/web-servers/linux-commands/">Linuxコマンド</a><a href="https://learn.utcode.net/docs/web-servers/node-js/">はじめてのNode.js</a>を確認して、(Widnowsの方はWSLと、)Node.jsをインストールしてください。また、<a href="https://learn.utcode.net/docs/web-servers/git-github-init/">GitとGitHubのセットアップ</a>の内容も終わらせておいてください。</p>
-    <p><a href="https://learn.utcode.net/docs/web-servers/module/">モジュールとnpm</a><a href="https://learn.utcode.net/docs/web-servers/git/">Gitを用いたバージョン管理</a><a href="https://learn.utcode.net/docs/web-servers/github/">GitとGithubを用いた共同開発</a>の知識も使うので、わからなくなったら参考にしてください。</p>
+    <p style:margin-bottom="30px"><a href="https://learn.utcode.net/docs/web-servers/module/">モジュールとnpm</a><a href="https://learn.utcode.net/docs/web-servers/git/">Gitを用いたバージョン管理</a><a href="https://learn.utcode.net/docs/web-servers/github/">GitとGithubを用いた共同開発</a>の知識も使うので、わからなくなったら参考にしてください。</p>
+    <h2>1.GitHubのリポジトリを作成する</h2>
+    <p>まず、GitHubにサインインし、画面右上の+ボタン(下の写真の赤丸)を押し、New repositoryを選択してください。</p>
+    <img src={picture1} alt="githubの画面1" style="width:95%; max-width:700px; height:auto; margin-right:10px"/>
 </div>
 
 <style>
