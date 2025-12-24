0040628282a698e47b9d5a6d86d7d3cdc16d6117
Kanata844 <ta3wata@gmail.com>
2025-12-24 20:10:56 +0900

    できててほしいが多分無理

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <footer class="container" style={style}>
-    <p><a href="https://web-design-tutorial.kanata844.workers.dev/license.md">サードパーティライセンス</a></p>
+    <p><a href={resolve("/license.json")}>サードパーティライセンス</a></p>
 </footer>
 
 <style>
