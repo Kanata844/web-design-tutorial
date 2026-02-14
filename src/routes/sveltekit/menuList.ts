a3a33f2857845fb9dbcb5657668a2851f3c63d52
Kanata844 <ta3wata@gmail.com>
2026-02-14 15:08:13 +0900

    とりあえずsveltekitは仮で完成

--- src/routes/sveltekit/menuList.ts
+++ src/routes/sveltekit/menuList.ts
@@ -24,5 +24,21 @@ export const menuList = [
     {
         title: "コンポーネントを利用する",
         url: resolve("/sveltekit/page6")
+    },
+    {
+        title: "Eachブロック、Ifブロック",
+        url: resolve("/sveltekit/page7")
+    },
+    {
+        title: "ページ遷移、画像の使い方",
+        url: resolve("/sveltekit/page8")
+    },
+    {
+        title: "自作コンポーネントの入れ子",
+        url: resolve("/sveltekit/page9")
+    },
+    {
+        title: "ちょっとした小技",
+        url: resolve("/sveltekit/page10")
     }
 ]
