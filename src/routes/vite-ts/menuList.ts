93c7b3c2a6849b907d3560de832d4f7e5704c164
Kanata844 <ta3wata@gmail.com>
2026-02-17 16:41:37 +0900

    こんどこそリンクを修正

--- src/routes/vite-ts/menuList.ts
+++ src/routes/vite-ts/menuList.ts
@@ -3,10 +3,10 @@ import { resolve } from "$app/paths";
 export const menuList = [
     {
         title: "ページ1",
-        url: resolve("/sveltekit/page1"),
+        url: resolve("/vite-ts/page1"),
     },
     {
         title: "ページ2",
-        url: resolve("/sveltekit/page2"),
+        url: resolve("/vite-ts/page2"),
     }
 ]
